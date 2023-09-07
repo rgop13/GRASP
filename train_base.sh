1998f4b556dd108b9e6470b3850182b07fe486e5
rgop13 <s0ny@korea.ac.kr>
2023-09-07 13:48:33 +0900

    Updated

--- train_base.sh
+++ train_base.sh
@@ -1,13 +1,13 @@
 python train.py \
---config_file GRASP-base \
---save_dirpath checkpoint/grasp_base \
---data_dir data/datav1 \
---process_name GRASP_base \
---mode train
+  --config_file GRASP-base \
+  --save_dirpath checkpoint/grasp_base \
+  --data_dir data/datav1 \
+  --process_name GRASP_base \
+  --mode train
 
 python train.py \
---config_file GRASP-base \
---save_dirpath checkpoint/grasp_base \
---data_dir data/datav1 \
---process_name GRASP_base \
---mode evaluation
+  --config_file GRASP-base \
+  --save_dirpath checkpoint/grasp_base \
+  --data_dir data/datav1 \
+  --process_name GRASP_base \
+  --mode evaluation
--- train_large.sh
+++ train_large.sh
@@ -1,13 +1,13 @@
 python train.py \
---config_file GRASP-large \
---save_dirpath checkpoint/grasp_large \
---data_dir data/datav1 \
---process_name GRASP_large \
---mode train
+  --config_file GRASP-large \
+  --save_dirpath checkpoint/grasp_large \
+  --data_dir data/datav1 \
+  --process_name GRASP_large \
+  --mode train
 
 python train.py \
---config_file GRASP-large \
---save_dirpath checkpoint/grasp_large \
---data_dir data/datav1 \
---process_name GRASP_large \
---mode evaluation
+  --config_file GRASP-large \
+  --save_dirpath checkpoint/grasp_large \
+  --data_dir data/datav1 \
+  --process_name GRASP_large \
+  --mode evaluation
